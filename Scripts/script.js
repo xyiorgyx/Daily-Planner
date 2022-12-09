b1611ffc738560ca6285c542f1c8edff156adffb
xyiorgyx <112989524+xyiorgyx@users.noreply.github.com>
2022-12-09 13:44:13 -0500

    Header
    The Date has been added and formatted to the header of the page.

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,7 +1,15 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
+
+
+
 $(function () {
+  const saveButton = $('button');
+
+  saveButton.on('click', function(){
+    var eventContent = JSON.stringify
+
+
+  });
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -21,3 +29,9 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+console.log(dayjs())
+const dateDiv = $('#currentDay')
+const date = dayjs().format('dddd MMMM D, YYYY')
+dateDiv.append(date)
+
+ console.log(dayjs().hour())
